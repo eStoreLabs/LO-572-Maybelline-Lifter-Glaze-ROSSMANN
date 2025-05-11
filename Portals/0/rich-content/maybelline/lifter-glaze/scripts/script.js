fd4933b582a278af4ea2196d9bcd0d10b987b1f8
WitchDevelops <d.n.wojewska@gmail.com>
2025-05-11 21:26:33 +0200

    feat: minify images, adjust directory structure, fix access paths, minify styles

---
+++ Portals/0/rich-content/maybelline/lifter-glaze/scripts/script.js
@@ -0,0 +1,18 @@
+(() => {
+  if (!document.querySelector("#es-rc.es-swiper")) {
+    const swiper = new Swiper("#es-rc .es-swiper", {
+      loop: false,
+      autoHeight: false,
+
+      pagination: {
+        el: "#es-rc .es-swiper1-pagination",
+        clickable: true,
+      },
+
+      navigation: {
+        nextEl: "#es-rc .es-swiper1-button-next",
+        prevEl: "#es-rc .es-swiper1-button-prev",
+      },
+    });
+  }
+})();
